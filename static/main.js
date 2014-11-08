8bac81894e3199b2bc1533a141fe6dba173afe6b
AndrewShao <shaoandrew@berkeley.edu>
2014-11-08 13:54:13 -0800

    Volume Changerinos

--- static/main.js
+++ static/main.js
@@ -1,13 +1,26 @@
+
 var pointers = {};
 var music = document.getElementById("theSong");
+music.volume = 0.7
 Leap.loop(function(frame){
 	music.playbackRate = 1;
 	if(frame.hands.length > 0){
+		console.log(music.volume);
 		var index = frame.hands[0].indexFinger;
-		if(index.extended == false){
-			console.log(frame.hands[0].grabStrength)
+		if(index.extended == false || Math.abs(frame.hands[0].palmNormal[1]) > .7){
+			
 			if(frame.hands[0].grabStrength > 0.8){
 				music.playbackRate = 0;
+			}
+			else if(frame.hands[0].palmNormal[1] < 0 && frame.hands[0].palmVelocity[1] < -70){
+				if(music.volume-0.01 > .01){
+					music.volume = music.volume -0.015;
+				}
+			}
+			else if(frame.hands[0].palmNormal[1] > 0 && frame.hands[0].palmVelocity[1] > 70){
+				if(music.volume+0.01 < .99){
+					music.volume = music.volume + 0.015;
+				}
 			}
 
 		}
