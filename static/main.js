b57452e7db7de3bda708695730db806e772c8074
WonPark <won.park19@gmail.com>
2014-11-08 19:52:46 -0800

    phil's addition

--- static/main.js
+++ static/main.js
@@ -53,6 +53,35 @@ Leap.loop(function(frame){
 	music.playbackRate = 0;
 });
 
+//Phil's Code
+
+Leap.loop(function(frame){
+	var currhandposition = frame.hands.palmPosition
+	if(frame.hands.length === 0){
+		music.playbackRate = 0;
+	}
+
+	if ((currhandposition[0] < (left box right boundary) && (currhandposition[1] < (left box top boundary)) && (currhandposition[1] > (left box bottom boundary)))
+		|| ((currhandposition[0] > (right box left boundary) && (currhandposition[1] < (right box top boundary)) && (currhandposition[1] > (right box bottom boundary))))
+		|| ((currhandposition[1] > (top box bottom boundary) && (currhandposition[0] > (top box left boundary)) && (currhandposition[0] < (top box right boundary))))
+		|| ((currhandposition[1] < (bottom box top boundary) && (currhandposition[0] > (bottom box left boundary)) && (currhandposition[0] < (bottom box right boundary))))
+		){
+		var currframerate = my_controller.frames(0)
+		float framedifference = currframerate - previousframerate
+		previousframerate = currframerate
+		float conductorbpm = 60 / framedifference
+		float newspeed = conductorbpm / 154
+		music.playbackRate = newspeed
+	}
+	else {
+		music.playbackRate = 1;
+	}
+	frame.hands.forEach(function(hand, index){
+		var pointer = (pointers[index] || (pointers[index] = new Pointer()))
+		pointer.setPosition(hand.screenPosition());
+	});
+}).use('screenPosition', {scale: 0.25});
+
 
 //creates new pointer
 var Pointer = function(){
